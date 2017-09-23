e44cd103c4c932a5857965f07ba09a8d50989ab7
Shahrukh Shahid <shah.khokhar@hotmail.com>
2017-09-23 19:57:17 +0500

    model typo

--- server/boot/accessKey.js
+++ server/boot/accessKey.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = function(app) {
-    var Accesskey = app.models.Accesskey;
+    var Accesskey = app.models.Access_key;
     var IDS = ['34562345', '09893102', '76092435', '33908412', '46509380'];
     for (var index = 0; index < IDS.length; index++) {
         var obj = {
