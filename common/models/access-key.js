952b8457908a8f4b4ba4c1f7ff1d18eccca983e6
Shahrukh Shahid <shah.khokhar@hotmail.com>
2017-09-23 20:34:43 +0500

    remote-method verify

--- common/models/access-key.js
+++ common/models/access-key.js
@@ -1,5 +1,17 @@
 'use strict';
 
 module.exports = function(Accesskey) {
-
+    Accesskey.verify = function(key, callback) {
+        Accesskey.find({ where: { key: key } }, function(err, success) {
+            if (!err) {
+                if (success.length > 0 && success[0].status == true) {
+                    callback(null, "true");
+                } else {
+                    callback(null, "false");
+                }
+            } else {
+                callback(err, null);
+            }
+        })
+    };
 };
