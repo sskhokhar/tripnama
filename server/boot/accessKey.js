e3388b9185fd3ebfd7cc9c91eb237f2b1cc8655c
Shahrukh Shahid <shah.khokhar@hotmail.com>
2017-09-23 19:46:01 +0500

    remote method

--- server/boot/accessKey.js
+++ server/boot/accessKey.js
@@ -2,12 +2,21 @@
 
 module.exports = function(app) {
     var Accesskey = app.models.Accesskey;
-    var IDS = [];
-    for (var index = 0; index < 11; index++) {
-        setInterval(function() {
-            var a = new Date();
-            console.log(a.getTime());
-            IDS.push(a.getTime());
-        }, 2000);
+    var IDS = ['34562345', '09893102', '76092435', '33908412', '46509380'];
+    for (var index = 0; index < IDS.length; index++) {
+        var obj = {
+            id: IDS[i],
+            startDate: new Date(),
+            endDate: new Date() + 10,
+            status: false
+        }
+        Accesskey.create(obj, function(err, succ) {
+            if (!err) {
+                console.log(succ);
+            } else {
+                console.log(err);
+            }
+        });
     }
+
 };
