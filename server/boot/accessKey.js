0114b2ba3483f61b6d22eefb8d3aaf31cfb3c4be
Shahrukh Shahid <shah.khokhar@hotmail.com>
2017-09-23 19:53:27 +0500

    findOrCreate accesskey

--- server/boot/accessKey.js
+++ server/boot/accessKey.js
@@ -5,16 +5,16 @@ module.exports = function(app) {
     var IDS = ['34562345', '09893102', '76092435', '33908412', '46509380'];
     for (var index = 0; index < IDS.length; index++) {
         var obj = {
-                id: IDS[index],
-                status: false
+            id: IDS[index],
+            status: false
+        }
+        Accesskey.findOrCreate(obj, function(err, succ) {
+            if (!err) {
+                console.log(succ);
+            } else {
+                console.log(err);
             }
-            // Accesskey.create(obj, function(err, succ) {
-            //     if (!err) {
-            //         console.log(succ);
-            //     } else {
-            //         console.log(err);
-            //     }
-            // });
+        });
     }
 
 };
