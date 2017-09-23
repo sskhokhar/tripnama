c2320c594c01c88a7a26096fde7315f7d2de7d8d
Shahrukh Shahid <shah.khokhar@hotmail.com>
2017-09-23 19:30:10 +0500

    bootMethod

---
+++ server/boot/accessKey.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = function(app) {
+    var Accesskey = app.models.Accesskey;
+    var IDS = [];
+    for (var index = 0; index < 11; index++) {
+        setInterval(function() {
+            var a = new Date();
+            console.log(a);
+            IDS.push(a.getMilliseconds);
+        }, 2000);
+    }
+
+};
